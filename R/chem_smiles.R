a41b1f10bc454a791e03fb8f684f36ceb05cf9e9
Jack Davison <jd1184@york.ac.uk>
2021-04-10 22:36:53 +0100

    update_data

--- R/chem_smiles.R
+++ R/chem_smiles.R
@@ -2,7 +2,7 @@
 #'
 #' A dataset providing example simplified molecular-input line-entry system ("SMILES") strings for use in chemdesr.
 #'
-#' @format A data frame with 53940 rows and 10 variables:
+#' @format A data frame with 42 rows and 2 variables:
 #' \describe{
 #'   \item{compound_id}{Descriptive names of 42 chemical compounds.}
 #'   \item{smile}{The corrsponding SMILES strings of the compounds.}
